9c524afe6df986e20853af3ba5b36647c054a99c
seeren <consultant@seeren.fr>
2022-06-14 15:27:34 +0200

    Use transcient

--- demo/wordpress/wp-content/plugins/wp-easy-redirection/templates/admin/menu/redirection_section/show.html.php
+++ demo/wordpress/wp-content/plugins/wp-easy-redirection/templates/admin/menu/redirection_section/show.html.php
@@ -1,3 +1,6 @@
 <div class="wrap">
+    <?php if ($last_update) : ?>
+        <p>Dernière modification: <?= $last_update ?></p>
+    <?php endif ?>
     <p>Entrez à gauche un slug qui n'existe plus et qui à la navigation provoquera une redirection vers le slug précisé à droite.</p>
 </div>
