d2586d79af6e4dbbc251cf8d314d4af991571036
seeren <consultant@seeren.fr>
2022-06-14 15:26:59 +0200

    Translate

--- demo/wordpress/wp-content/plugins/wp-easy-redirection/templates/admin/menu/top_menu/show.html.php
+++ demo/wordpress/wp-content/plugins/wp-easy-redirection/templates/admin/menu/top_menu/show.html.php
@@ -1,6 +1,6 @@
 <div class="wrap">
     <h1><?= $title ?></h1>
-    <p>Quand vous décidez de modifier le slug d'un article ou d'une page alors les liens vers les anciens slugs tomberons sur une page 404.</p>
+    <p><?= __('When you decide to change the slug of an article or a page then the links to the old slugs will fall on a 404 page', 'wp-easy-redirection') ?>.</p>
     <div class="card">
         <h2>Redirections</h2>
         <p>Créez des redirections de vos anciennes pages ou articles vers les nouvelles!</p>
